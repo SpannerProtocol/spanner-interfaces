bb4c83bb7c9d336670430e0ad1294a1b9b1a1c99
NatPopov <natpopov007@gmail.com>
2021-01-29 20:19:18 +0800

    rename dex to primitives

--- definitions.ts
+++ definitions.ts
@@ -1,4 +1,4 @@
-export { default as dex } from './dex/definitions'
+export { default as dex } from './primitives/definitions'
 export { default as pocModule } from './pocModule/definitions'
 export { default as common } from './common/definitions'
 export { default as rewards } from './rewards/definitions'
