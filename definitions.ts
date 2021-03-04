b6bcbeb6a9cd23a9e7cf92bdb63093a83a405ab1
NatPopov <natpopov007@gmail.com>
2021-03-04 15:59:00 +0800

    fix bug

--- definitions.ts
+++ definitions.ts
@@ -1,5 +1,5 @@
 export { default as primitives } from './primitives/definitions'
-export { default as pocModule } from './bulletTrain/definitions'
+export { default as bulletTrain } from './bulletTrain/definitions'
 export { default as common } from './common/definitions'
 export { default as rewards } from './rewards/definitions'
 export { default as dex } from './dex/definitions'
