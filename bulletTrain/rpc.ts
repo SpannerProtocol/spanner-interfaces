a94e94bc7e47610d40311989272407921279881e
NatPopov <natpopov007@gmail.com>
2021-03-05 13:22:51 +0800

    update dpos rpc

--- bulletTrain/rpc.ts
+++ bulletTrain/rpc.ts
@@ -17,5 +17,20 @@ export default {
         type: 'Vec<(LockDropIndex, LockDropInventoryIndex)>',
       },
     },
+    getDposOfAccount: {
+      description: 'Get dpos of account',
+      params: [
+        {
+          name: 'account',
+          type: 'AccountId',
+        },
+        {
+          name: 'at',
+          type: 'Hash',
+          isOptional: true,
+        },
+      ],
+      type: 'Vec<DpoIndex>',
+    },
   },
 };
