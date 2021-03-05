de4fc7296884e898c0fc6993fa8bac3fb7ec1dc8
NatPopov <natpopov007@gmail.com>
2021-03-05 14:01:11 +0800

    fix bug

--- bulletTrain/rpc.ts
+++ bulletTrain/rpc.ts
@@ -16,21 +16,21 @@ export default {
         ],
         type: 'Vec<(LockDropIndex, LockDropInventoryIndex)>',
       },
-    },
-    getDposOfAccount: {
-      description: 'Get dpos of account',
-      params: [
-        {
-          name: 'account',
-          type: 'AccountId',
-        },
-        {
-          name: 'at',
-          type: 'Hash',
-          isOptional: true,
-        },
-      ],
-      type: 'Vec<DpoIndex>',
+      getDposOfAccount: {
+        description: 'Get dpos of account',
+        params: [
+          {
+            name: 'account',
+            type: 'AccountId',
+          },
+          {
+            name: 'at',
+            type: 'Hash',
+            isOptional: true,
+          },
+        ],
+        type: 'Vec<DpoIndex>',
+      },
     },
   },
 };
