b9a64cd8568ff28bb56a982d8296a21d39d9ca39
NatPopov <natpopov007@gmail.com>
2021-03-05 13:31:51 +0800

    reolve merge conflict

--- bulletTrain/rpc.ts
+++ bulletTrain/rpc.ts
@@ -16,26 +16,6 @@ export default {
         ],
         type: 'Vec<(LockDropIndex, LockDropInventoryIndex)>',
       },
-<<<<<<< HEAD
-      getDposOfAccount: {
-        description: 'Get dpos of account',
-        params: [
-          {
-            name: 'account',
-            type: 'AccountId',
-          },
-          {
-            name: 'at',
-            type: 'Hash',
-            isOptional: true,
-          },
-        ],
-        type: 'Vec<(DpoIndex, DpoMemberIndex)>',
-      },
-    },
-  },
-}
-=======
     },
     getDposOfAccount: {
       description: 'Get dpos of account',
@@ -54,4 +34,3 @@ export default {
     },
   },
 };
->>>>>>> 37895a44f8b0b2efdf3200e67a039011e16f1dfe
